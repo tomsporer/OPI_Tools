51ef79b1a9e9b7202ac06cf3410fb1dbce3f4bd1
Helge Mathee <helge.mathee@fabricengine.com>
2017-02-14 12:59:41 +0100

    extend project tool to perform workspace setup

--- tools/Projects/CreateProjectTool.py
+++ tools/Projects/CreateProjectTool.py
@@ -35,4 +35,76 @@ class CreateProjectTool(DataBaseTool):
     task3D = db.createNew('Task', project=project, name='3D')
     taskComposite = db.createNew('Task', project=project, name='Composite')    
 
-    
+    workspaceContent = """
+// Tom Sporer Project Definition - Maya 2017
+
+workspace -fr "STL_ATF" "Models";
+workspace -fr "DAE_FBX" "Models";
+workspace -fr "NX_ATF" "Models";
+workspace -fr "OBJ" "Models";
+workspace -fr "FBX export" "Models";
+workspace -fr "BIF" "Models";
+workspace -fr "DAE_FBX export" "Models";
+workspace -fr "CATIAV5_ATF" "Models";
+workspace -fr "ASS Export" "Models";
+workspace -fr "move" "Models";
+workspace -fr "FBX" "Models";
+workspace -fr "CATIAV4_ATF" "Models";
+workspace -fr "eps" "Models";
+workspace -fr "translatorData" "Models";
+workspace -fr "SAT_ATF" "Models";
+workspace -fr "PROE_ATF" "Models";
+workspace -fr "INVENTOR_ATF" "Models";
+workspace -fr "STEP_ATF" "Models";
+workspace -fr "IGES_ATF" "Models";
+workspace -fr "ASS" "Models";
+workspace -fr "Alembic" "Models";
+workspace -fr "illustrator" "Models";
+workspace -fr "diskCache" "Models";
+workspace -fr "OBJexport" "Models";
+workspace -fr "JT_ATF" "Models";
+workspace -fr "sound" "Models";
+workspace -fr "audio" "Models";
+workspace -fr "templates" "Models";
+
+workspace -fr "images" "Images";
+workspace -fr "sourceImages" "Images";
+workspace -fr "clips" "Images";
+workspace -fr "3dPaintTextures" "Images/3dPaintTextures";
+
+workspace -fr "fluidCache" "Cache/nCache/fluid";
+workspace -fr "fileCache" "Cache/nCache";
+workspace -fr "particles" "Cache/particles";
+workspace -fr "bifrostCache" "Cache/bifrost";
+
+workspace -fr "movie" "Render";
+workspace -fr "renderData" "Render";
+workspace -fr "shaders" "Render/shaders";
+workspace -fr "iprImages" "Render/iprImages";
+workspace -fr "depth" "Render/depth";
+workspace -fr "furShadowMap" "Render/fur/furShadowMap";
+workspace -fr "furFiles" "Render/fur/furFiles";
+workspace -fr "furEqualMap" "Render/fur/furEqualMap";
+workspace -fr "furImages" "Render/fur/furImages";
+workspace -fr "furAttrMap" "Render/fur/furAttrMap";
+
+workspace -fr "scripts" "Scripts";
+workspace -fr "mel" "Scripts";
+
+workspace -fr "offlineEdit" "{0}/Edits";
+workspace -fr "mayaAscii" "{0}";
+workspace -fr "autoSave" "{0}/Autosave";
+workspace -fr "mayaBinary" "{0}";
+workspace -fr "scene" "{0}";
+
+workspace -fr "timeEditor" "Time Editor";
+
+workspace -fr "sceneAssembly" "sceneAssembly";
+workspace -fr "teClipExports" "Time Editor/Clip Exports";
+""".format(task3D.location.partition('/')[2])
+
+    projectPath = db.getPath(project.location)
+    with open(os.path.join(projectPath, 'workspace.mel'), 'wb') as f:
+      f.write(workspaceContent)
+
+    self.host.apis['pymel'].core.mel.eval('setProject "%s"' % projectPath.replace("\\", '/'))
