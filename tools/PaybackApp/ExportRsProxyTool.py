83c91323821ee520ebd9825cb8ec3ade572f63bf
DominicWerner <dominic_werner@gmx.de>
2017-07-03 10:13:07 +0200

    code clean up

--- tools/PaybackApp/ExportRsProxyTool.py
+++ tools/PaybackApp/ExportRsProxyTool.py
@@ -28,15 +28,6 @@ class ExportRsProxyTool(DataBaseTool):
     self.__project = db.queryOne("project", name="Payback_App")
     self.__projectPath = db.getPath(self.__project.location)
 
-    # self.args.addStaticText("\tExport rsProxy \t \t \t")
-    # self.args.addSpacer(13)
-    # self.args.add(name="project", type="instance", template="project", comboSqlQuery="SELECT * FROM project WHERE project.name == 'Payback_App'", enabled=False, hidden=True)
-    # self.args.add(name="name", type="str", label="Name", expression="[A-Za-z]+[A-Za-z0-9]*", value=None, enabled=False)
-    # # self.args.addSpacer(10,0)
-    # self.args.beginRow("Frame Range")
-    # self.args.add(name="rangeStart", type="int", label="start", value=1)
-    # self.args.add(name="rangeEnd", type="int", label="end", value=2)
-    # self.args.endRow()
 
 
   def getAlembicNode(self, rootLocator):
@@ -51,38 +42,6 @@ class ExportRsProxyTool(DataBaseTool):
           if "AlembicNode" in c:
             return c
             break
-
-
-  # def preexecute(self, **args):
-  #   maya = self.host.apis['maya']
-  #   cmds = maya.cmds
-
-  #   # get name from selected locator
-  #   loc = cmds.ls(selection=True)[0]
-  #   pName = loc.split("_")[2]
-  #   self.args.get("name").value = pName
-    
-  #   db = self.host.apis['db']
-  #   QtWidgets = self.host.apis["QtWidgets"]
-  #   pName = self.args.getValue("name")
-
-  #   # fileExists = db.query("pointee_rsproxy", name=pName)
-  #   # if fileExists != []:
-  #   #   msgBox = QtWidgets.QMessageBox
-  #   #   msgBox.warning(None, "File already exists", "A Proxy with that name already exists.\n! Overriding will cause problems !\nPlease choose a different name (or manually delete the old files before continuing).")
-    
-  #   # # Get framerange from playback range
-  #   # self.args.get("rangeStart").value = cmds.playbackOptions( q=True, minTime=True )
-  #   # self.args.get("rangeEnd").value = cmds.playbackOptions( q=True, maxTime=True )
-
-  #   # Get framerange from alembic node
-  #   abcNode = self.getAlembicNode(loc)
-  #   self.args.get("rangeStart").value = cmds.getAttr(abcNode + ".startFrame")
-  #   self.args.get("rangeEnd").value = cmds.getAttr(abcNode + ".endFrame")
-
-  #   if len(cmds.ls(selection=True)) == 0:
-  #     raise OPIException("Nothing selected. Please select objects/sets and retry")
-
 
 
   def executeMaya(self):
@@ -112,5 +71,3 @@ class ExportRsProxyTool(DataBaseTool):
       
       print "# INFO: executing \"" + rsProxyExportString + "\" where selection=" + str(cmds.ls(selection=True))
       mel.eval(rsProxyExportString)
-    # db.getOrCreateNew("pointee_rsproxy", project=self.__project, name=pName, createEmptyFile=False)
-
