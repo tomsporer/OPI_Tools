84cb845489a79ed1b5a6bc614c0e60a42b534c94
Helge Mathee <helge.mathee@fabricengine.com>
2017-08-03 13:59:36 +0200

    first steps towards hair system

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#
+# Copyright 2010-2014 Fabric Software Inc. All rights reserved.
+#
+
+export FABRIC_EXTS_PATH=$FABRIC_EXTS_PATH:$PWD/fabric/exts
+export FABRIC_DFG_PATH=$FABRIC_DFG_PATH:$PWD/fabric/presets
