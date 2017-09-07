2be8105775697435298d285ecf9c81e3c4145003
DominicWerner <dominic_werner@gmx.de>
2017-09-07 19:36:07 +0200

    added dialog window to findDuplicatesTool

--- tools/Rigging/RigFindDuplicateNamesTool.py
+++ tools/Rigging/RigFindDuplicateNamesTool.py
@@ -3,10 +3,6 @@
 # Copyright (c) 2016, Tom Sporer. All rights reserved.
 #
 
-import os
-import re
-import sys
-
 from opi.tools.tool import Tool
 from opi.common.opiexception import OPIException
 
@@ -23,7 +19,7 @@ class RigFindDuplicateNamesTool(Tool):
     self._noUI = True
 
 
-  def execute(self):
+  def executeMaya(self):
     maya = self.host.apis['maya']
     cmds = maya.cmds
 
@@ -34,13 +30,24 @@ class RigFindDuplicateNamesTool(Tool):
     for s in sel:
       objs.append(s.split("|")[-1])
 
-    duplicates = objs
-    uniqueList = list(set(objs))
-    for item in uniqueList:
-      duplicates.remove(item)
-        
-    if duplicates != []:
+    duplicates = {name : objs.count(name) for name in objs}
+    for dupl in duplicates.keys():
+      if duplicates[dupl] == 1:
+        duplicates.pop(dupl)
+
+    if duplicates.keys() != []:
       print "# INFO: Duplicate Names Found:"
       print duplicates
+      dialogDupls = ""
+      for dupl in duplicates.keys():
+        dialogDupls += "\"%s\" - %s times\n" %(dupl, duplicates[dupl])
+
+      dialog = cmds.confirmDialog(title="Duplicate Names Found!", message="Duplicate Names Found!\n\n" + dialogDupls, button=["Ok", "Select Duplicates"], defaultButton="Ok", cancelButton="Ok", dismissString="Ok")
+      if dialog == "Select Duplicates":
+        cmds.select(clear=True)
+        for dupl in duplicates.keys():
+          cmds.select("*|" + dupl, add=True)
+
     else:
       print "# INFO: No duplicates found"
+      dialog = cmds.confirmDialog(title="No Duplicates Found.", message="No duplicate names found.", button=["Ok"], defaultButton="Ok", cancelButton="Ok", dismissString="Ok")
