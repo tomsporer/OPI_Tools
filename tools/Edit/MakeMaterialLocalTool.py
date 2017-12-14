f363f7ebeccdcd8001213ecfa28e4962f2f4ae99
DominicWerner <dominic_werner@gmx.de>
2017-12-14 18:33:50 +0100

    added tool to make a shared material on an object local

---
+++ tools/Edit/MakeMaterialLocalTool.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016, Tom Sporer. All rights reserved.
+#
+
+
+from opi.tools.tool import Tool
+from opi.common.opiexception import OPIException
+
+class MakeMaterialLocalTool(Tool):
+
+  ToolName = 'MakeMaterialLocal'
+  ToolLabel = 'Make Material local'
+  ToolCommand = 'makemateriallocal'
+  ToolDescription = 'Make shared Material unique and connect to Object'
+  ToolTooltip = 'Make shared Material unique and connect to Object'
+
+  def __init__(self, host):
+    super (MakeMaterialLocalTool, self).__init__(host)
+    self._noUI = True
+
+
+  # def initialize(self, **args):
+
+
+  def executeMaya(self):
+    maya = self.host.apis['maya']
+    cmds = maya.cmds
+
+    def getSGFromObject(obj):
+      shape = cmds.listRelatives(obj, children=True, shapes=True, path=True)[0]
+      mat = cmds.listConnections(shape, type="shadingEngine")[0]
+      return mat
+
+    sel = cmds.ls(selection=True)
+    for s in sel:
+      sharedSG = getSGFromObject(s)
+      localSG = cmds.duplicate(sharedSG, upstreamNodes=True)[0]
+      print localSG
+      cmds.sets(s, forceElement=localSG)
