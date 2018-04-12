de975774f4f18c1a8da93fce38173ab5ee55f715
DominicWerner <dominic_werner@gmx.de>
2018-04-12 17:21:03 +0200

    increment now also works with version numbers that are not at the end of the file name

--- tools/File/IncrementAndSaveTool.py
+++ tools/File/IncrementAndSaveTool.py
@@ -23,33 +23,62 @@ class IncrementAndSaveTool(DataBaseTool):
   # def initialize(self, **args):
 
 
-  def increment(self, scenePath):
+  # def increment(self, scenePath):
 
-    sceneFolder = os.path.split(scenePath)[0]
-    sceneFile = os.path.split(scenePath)[1]
+  #   sceneFolder = os.path.split(scenePath)[0]
+  #   sceneFile = os.path.split(scenePath)[1]
+  #   sceneFileName, sceneFileExt = os.path.splitext(sceneFile)
+
+  #   if sceneFileName[-1].isdigit() == True: # check if there is already a version number at the end and increment if true
+  #     i = 1
+  #     while sceneFileName[-i].isdigit() == True:
+  #       lastDigit = i
+  #       i = i + 1
+  #     oldFileVersion = sceneFileName[-lastDigit:]
+  #     newFileVersion = str(int(oldFileVersion) + 1).rjust(len(oldFileVersion), "0")
+  #     constantFileName = str(sceneFileName[:-lastDigit])
+  #   else:
+  #     constantFileName = str(sceneFileName)
+  #     oldFileVersion = ""
+  #     newFileVersion = "_01"
+
+  #   savePath = os.path.join(sceneFolder, constantFileName + newFileVersion)
+  #   return savePath + sceneFileExt
+
+
+  def increment(self, sceneFile):
+
     sceneFileName, sceneFileExt = os.path.splitext(sceneFile)
 
-    if sceneFileName[-1].isdigit() == True: # check if there is already a version number at the end and increment if true
-      i = 1
-      while sceneFileName[-i].isdigit() == True:
-        lastDigit = i
-        i = i + 1
-      oldFileVersion = sceneFileName[-lastDigit:]
-      newFileVersion = str(int(oldFileVersion) + 1).rjust(len(oldFileVersion), "0")
-      constantFileName = str(sceneFileName[:-lastDigit])
+    for i in list(reversed(range(0, len(sceneFileName)))):
+      if sceneFileName[i].isdigit():
+        suffix = sceneFileName[i+1:]
+        sansSuffix = sceneFileName[:i+1]
+        i = 1
+        while sansSuffix[-i].isdigit() == True:
+          lastDigit = i
+          i = i + 1
+        oldFileVersion = sansSuffix[-lastDigit:]
+        newFileVersion = str(int(oldFileVersion) + 1).rjust(len(oldFileVersion), "0")
+        constantFileName = str(sansSuffix[:-lastDigit])
+        break
     else:
       constantFileName = str(sceneFileName)
       oldFileVersion = ""
       newFileVersion = "_01"
+      suffix = ""
 
-    savePath = os.path.join(sceneFolder, constantFileName + newFileVersion)
-    return savePath + sceneFileExt
+    newSceneFile = constantFileName + newFileVersion + suffix + sceneFileExt
+    return newSceneFile
+
 
   def executeMaya(self):
 
     maya = self.host.apis['maya']
     scenePath = maya.cmds.file(q = True, sceneName = True)
-    savePath = self.increment(scenePath)
+    sceneDir, sceneFile = os.path.split(scenePath)
+    incrSceneFile = self.increment(sceneFile)
+    savePath = os.path.join(sceneDir, incrSceneFile)
 
     if os.path.exists(savePath) == True:
       QtWidgets = self.host.apis["QtWidgets"]
